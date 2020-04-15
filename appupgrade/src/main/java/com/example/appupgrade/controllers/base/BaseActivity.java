8505b75dd20cf350af085af28182d7b30c802fd3
xiaoqin <xiaoqin@hpmont.com>
2020-04-15 13:55:17 +0800

    2.0.4

--- appupgrade/src/main/java/com/example/appupgrade/controllers/base/BaseActivity.java
+++ appupgrade/src/main/java/com/example/appupgrade/controllers/base/BaseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
 import android.view.KeyEvent;
 import android.view.MenuItem;
 
